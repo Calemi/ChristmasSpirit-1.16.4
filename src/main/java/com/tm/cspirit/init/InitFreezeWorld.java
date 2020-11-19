cd11687b3ce9b53c9cfd0b325a6d07e01745a249
Calemi <lemzsteamerson@gmail.com>
2020-11-19 07:38:55 -0500

    Fixed World Freezing

--- src/main/java/com/tm/cspirit/init/InitFreezeWorld.java
+++ src/main/java/com/tm/cspirit/init/InitFreezeWorld.java
@@ -12,33 +12,33 @@ public class InitFreezeWorld {
     public static void init() {
 
         if (CSConfig.worldGen.freezeWorld.get()) {
+            DeferredWorkQueue.runLater(() -> freezeBiomes("field_242423_j", "field_242526_h", "field_235052_p_"));
+            DeferredWorkQueue.runLater(() -> freezeBiomes("climate", "grassColorModifier", "effects"));
+        }
+    }
 
-            DeferredWorkQueue.runLater(() -> {
+    private static void freezeBiomes(String climateVar, String grassColorModifierVar, String effectsVar) {
 
-                for (Biome biome : ForgeRegistries.BIOMES) {
+        for (Biome biome : ForgeRegistries.BIOMES) {
 
-                    if (!CSConfig.worldGen.freezeOceans.get()) {
+            if (!CSConfig.worldGen.freezeOceans.get()) {
 
-                        if (biome.getRegistryName() != null && biome.getRegistryName().toString().contains("ocean")) {
-                            continue;
-                        }
-                    }
-
-                    Biome.Climate climate = new Biome.Climate(Biome.RainType.SNOW, -1, Biome.TemperatureModifier.NONE, 1);
-                    BiomeAmbience ambience = biome.getAmbience();
-                    BiomeAmbience.GrassColorModifier colorModifier = BiomeAmbience.GrassColorModifier.NONE;
-
-                    try {
-                        FieldUtils.writeField(biome, "climate", climate, true);
-                        FieldUtils.writeField(ambience, "grassColorModifier", colorModifier, true);
-                        FieldUtils.writeField(biome, "effects", ambience, true);
-                    }
-
-                    catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
+                if (biome.getRegistryName() != null && biome.getRegistryName().toString().contains("ocean")) {
+                    continue;
                 }
-            });
+            }
+
+            Biome.Climate climate = new Biome.Climate(Biome.RainType.SNOW, -1, Biome.TemperatureModifier.NONE, 1);
+            BiomeAmbience.GrassColorModifier colorModifier = BiomeAmbience.GrassColorModifier.NONE;
+            BiomeAmbience ambience = biome.getAmbience();
+
+            try {
+                FieldUtils.writeField(biome, climateVar, climate, true);
+                FieldUtils.writeField(ambience, grassColorModifierVar, colorModifier, true);
+                FieldUtils.writeField(biome, effectsVar, ambience, true);
+            }
+
+            catch (IllegalAccessException ignored) {}
         }
     }
 }
